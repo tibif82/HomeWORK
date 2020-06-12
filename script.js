0759bcecdebce37ff9c9a41e676568b7aabb5555
tibif82 <tibif82@gmail.com>
2020-06-12 22:56:56 +0300

    add first home work from JavaScript

---
+++ script.js
@@ -0,0 +1,64 @@
+// let name = prompt('Hello! What is your name?');
+// alert(`Hello ${name}`);
+
+// *****************************************
+
+// const CURENT = 2020;
+// let born = prompt('When you were born?');
+// let age = CURENT-born;
+// alert(`Your ${age}`);
+
+// *****************************************
+
+// let stage = prompt('Enter the side of the square');
+// let P = stage * 4;
+// alert (`P = ${P}`);
+
+// *****************************************
+
+// let r = prompt('Enter the radius of the circle');
+// let S = 3.14 * (r ** 2);
+// alert (`S = ${S}`);
+
+// ****************************************
+
+// let S = prompt('Specify the distance');
+// let t = prompt('Specify the time');
+// let V = S/t
+// alert (`V = ${V} km/h`);
+
+// ****************************************
+
+// let kurs = prompt('Specify the exchange rate');
+// let dollar = prompt('Enter the number');
+// let Euro = dollar/kurs;
+// alert(`Euro = ${Euro}`);
+
+// ****************************************
+
+// const SIZE = 820;
+// let capacity = prompt('Enter capacity');
+// let Scale = Math.floor(capacity/SIZE);
+// alert (`Scale = ${Scale}`);
+
+
+// ****************************************
+
+// let money = prompt('How much money do you have?');
+// let price = prompt('The price of one chocolate bar');
+// alert ('You buy ' + ' ' + Math.floor(money/price) + ' ' + 'Remainder' + ' ' + (money%price) + 'grn');
+
+// ****************************************
+
+// var x = 8765;
+// var y = 0;
+// for(; x; x = Math.floor(x / 10)) {
+//     y *= 10;
+//     y += x % 10;
+// }
+// console.log(y);
+
+// ****************************************
+
+// let deposit = prompt('Consider the amount of the deposit');
+// alert('Your proffit: ' + ' ' + ((deposit*0.05)/12)*2);
